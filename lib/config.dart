1935b68b15834eb5c4831fe8ed37a02617156d34
aloisius.mirel <aloisius.mirel@redtech.co.id>
2024-08-02 15:32:16 +0700

    update

--- lib/config.dart
+++ lib/config.dart
@@ -6,8 +6,8 @@ import 'api_end_points.dart';
 class Config {
   static final String baseUrl = ApiEndPoints.baseUrl;
   static int? userId;
-  String clientId = '3',
-      clientSecret = '1gEdDDj3uguYRHfkdjiHZ57OC00GNgBCSSr6LAUf',
+  String clientId = 'please add your clientId here',
+      clientSecret = 'please add your clientSecret here',
       copyright = '\u00a9',
       appName = 'app',
       version = 'V 1.7',
@@ -21,23 +21,36 @@ class Config {
   static int quantityPrecision = 2, currencyPrecision = 2, callLogSyncDuration = 30;
 
   //List of locale language code
-  List locale = ['en','id'];
-  String defaultLanguage = 'id';
+  List locale = ['en', 'ar', 'de', 'fr', 'es','tr','id','my','be','ch','it'];
+  String defaultLanguage = 'en';
 
   //List of locales included
   List<Locale> supportedLocales = [
     Locale('en', 'US'),
+    Locale('ar', ''),
+    Locale('de', ''),
+    Locale('fr', ''),
+    Locale('es', ''),
+    Locale('tr', ''),
     Locale('id', ''),
-
+    Locale('my', '')
   ];
 
   //dropdown items for changing language
   List<Map<String, dynamic>> lang = [
     {'languageCode': 'en', 'countryCode': 'US', 'name': 'English'},
+    {'languageCode': 'ar', 'countryCode': '', 'name': 'العربي'},
+    {'languageCode': 'de', 'countryCode': '', 'name': 'Deutsche'},
+    {'languageCode': 'fr', 'countryCode': '', 'name': 'Français'},
+    {'languageCode': 'es', 'countryCode': '', 'name': 'Española'},
+    {'languageCode': 'tr', 'countryCode': '', 'name': 'Türkçe'},
     {'languageCode': 'id', 'countryCode': '', 'name': 'Indonesian'},
-
+	{'languageCode': 'be', 'countryCode': '', 'name': 'Bengali'},
+    {'languageCode': 'ch', 'countryCode': '', 'name': 'chinese'},
+    {'languageCode': 'it', 'countryCode': '', 'name': 'italian'},
+    {'languageCode': 'my', 'countryCode': '', 'name': 'မြန်မာ'}
   ];
 
   //final initialPosition = LatLng(20.46752985010792, 82.92005813910752);
-  final String googleAPIKey = 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg';
+  final String googleAPIKey = 'AIzaSyDtorf5cQD5g7V4K2R0JVl8DcnnqiZS5Qw';
 }
